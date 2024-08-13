1fe246f4df99eefc358b0a288eb5728bf6fee12a
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-13 14:41:34 +0530

    Migrate to gradle

--- tour-service/src/main/java/com/example/tourservice/TourManagementService.java
+++ tour-service/src/main/java/com/example/tourservice/TourManagementService.java
@@ -3,20 +3,20 @@ package com.example.tourservice;
 import com.example.tourservice.utilities.TourRepository;
 
 public class TourManagementService {
-    private TourRepository tourRepository;
+  private TourRepository tourRepository;
 
-    
-    public TourManagementService(TourRepository tourRepository) {
-        this.tourRepository = tourRepository;
-        createTour("Big Sur Retreat", 750, true);
-        createTour( "Day Spa Package", 200, false);
-        createTour("Monterey to SantaBarbara Tour", 550, false);
-        createTour("Kids L.A. Tour", 100, true);
-        createTour("Islands of the Blue Dolphins Tour", 200, true);
-        createTour("Endangered Specie Expedition", 250, true);
-    } 
 
-    public Tour createTour(String title, Integer price, Boolean isKidFriendly) {
-        return tourRepository.save(new Tour(title, price, isKidFriendly));
-    }
+  public TourManagementService(TourRepository tourRepository) {
+    this.tourRepository = tourRepository;
+    createTour("Big Sur Retreat", 750, true);
+    createTour("Day Spa Package", 200, false);
+    createTour("Monterey to SantaBarbara Tour", 550, false);
+    createTour("Kids L.A. Tour", 100, true);
+    createTour("Islands of the Blue Dolphins Tour", 200, true);
+    createTour("Endangered Specie Expedition", 250, true);
+  }
+
+  public Tour createTour(String title, Integer price, Boolean isKidFriendly) {
+    return tourRepository.save(new Tour(title, price, isKidFriendly));
+  }
 }
