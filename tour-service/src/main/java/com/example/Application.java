1dbf74ff1a2aa45caec88280c062cc4d953eaaa7
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-13 12:37:18 +0530

    Migrate to gradle

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -5,18 +5,18 @@ import com.example.tourservice.TravelAgentService;
 import com.example.tourservice.utilities.TourRepository;
 
 public class Application {
-    public static void main(String[] args) {
-        // Must create Repo First
-        TourRepository repo = new TourRepository();
-        TravelAgentService agent = new TravelAgentService(repo);
-        
-        new TourManagementService(repo); //Creates tours in constructor
+  public static void main(String[] args) {
+    // Must create Repo First
+    TourRepository repo = new TourRepository();
+    TravelAgentService agent = new TravelAgentService(repo);
 
-        // Do not invoke agent method before manager creates base tours
-        System.out.println("\n******Explore California Tour Catalogue******");
-        agent.displayTours();
+    new TourManagementService(repo); //Creates tours in constructor
 
-        System.out.println("\n******Explore California Tour Kid Friendly Tours******");
-        agent.displayToursBy(true);
-    }
+    // Do not invoke agent method before manager creates base tours
+    System.out.println("\n******Explore California Tour Catalogue******");
+    agent.displayTours();
+
+    System.out.println("\n******Explore California Tour Kid Friendly Tours******");
+    agent.displayToursBy(true);
+  }
 }
