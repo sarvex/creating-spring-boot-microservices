62994af9c9c59c10095302d122b8f9266928952f
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-14 07:35:17 +0530

    Added Spring Context

---
+++ src/main/java/com/example/explorecalijpa/ExploreCaliJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.explorecalijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExploreCaliJpaApplication.class, args);
+  }
+
+}
